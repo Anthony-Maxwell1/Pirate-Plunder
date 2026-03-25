c69734cc63ee98dca578c3f08ad0eff9c6b8f3c5
Anthony Maxwell <amaxwell4013@gmail.com>
2026-03-25 21:52:25 +1100

    Reworking the audio system around names instead of ids

--- scripts/MusicBank.gd
+++ scripts/MusicBank.gd
@@ -2,46 +2,82 @@ extends Node
 
 enum Mode { THIRD, HALF, FULL, NONE }
 enum Automatic { DISABLED, BGMUSIC }
-@export var tracks: Array[AudioStream]
+
+@export var tracks: Array[Track]
+
 @export_category("Automatic")
 @export var automatic: Automatic = Automatic.DISABLED
+
 @export_group("Bgmusic")
 @export var repeatMode: Mode = Mode.THIRD
 
-const INVALID_ID := -50000
 
-var max_wait: int = 0 # maximum tracks until a track can be replayed again
+const INVALID_INDEX := -1
 
-var last_played: Array[String] = [] # last played tracks
+var max_wait: int = 0
+var last_played: Array[String] = [] # store track names
+var curr_id: int = INVALID_INDEX
 
-var curr_id: int = INVALID_ID
 
-func _not_in_last_played():
-	return tracks.filter(func(a): return not last_played.has(a.get_rid()))
+func _get_track_by_name(name: String) -> MusicTrack:
+	for t in tracks:
+		if t.name == name:
+			return t
+	return null
+
+
+func _not_in_last_played() -> Array[MusicTrack]:
+	return tracks.filter(func(t): return not last_played.has(t.name))
+
 
 func _automatic_bgmusic_pick_and_play():
-	var track = _not_in_last_played().pick_random()
+	var available := _not_in_last_played()
+
+	if available.is_empty():
+		last_played.clear()
+		available = tracks
+
+	var track: MusicTrack = available.pick_random()
+
+	# update history
 	if last_played.size() > max_wait:
 		last_played.pop_front()
-	last_played.append(track.get_rid())
-	curr_id = MusicManager.play(track)
 
-func play_track(rid: int = INVALID_ID) -> bool:
-	if rid != INVALID_ID:
-		if curr_id != INVALID_ID:
-			MusicManager.remove(curr_id)
-			curr_id = INVALID_ID
-		curr_id = MusicManager.play(tracks[tracks.find_custom(func(a): return a.get_rid() == rid)])
-		return true
-	return false
+	last_played.append(track.name)
+
+	# play
+	if curr_id != INVALID_INDEX:
+		MusicManager.remove(curr_id)
+
+	curr_id = MusicManager.play(track.stream)
+
+
+func play_track(name: String) -> bool:
+	var track := _get_track_by_name(name)
+	if track == null:
+		return false
+
+	if curr_id != INVALID_INDEX:
+		MusicManager.remove(curr_id)
+
+	curr_id = MusicManager.play(track.stream)
+	return true
+
 
 func _ready():
 	if automatic == Automatic.BGMUSIC:
-		if repeatMode == Mode.THIRD: max_wait = floor(tracks.size() / 3) # these will always be > 0, so nonegative protection is needed
-		if repeatMode == Mode.HALF: max_wait = floor(tracks.size() / 2)
-		if repeatMode == Mode.FULL: max_wait = max(tracks.size() - 1, 0) # make sure it doesn't go below 0
+		if repeatMode == Mode.THIRD:
+			max_wait = floor(tracks.size() / 3)
+		elif repeatMode == Mode.HALF:
+			max_wait = floor(tracks.size() / 2)
+		elif repeatMode == Mode.FULL:
+			max_wait = max(tracks.size() - 1, 0)
+		else:
+			max_wait = 0
+
 	_automatic_bgmusic_pick_and_play()
 
+
 func _process(delta: float):
-	if not curr_id or not MusicManager.get_playing(curr_id):
+	if curr_id == INVALID_INDEX or not MusicManager.get_playing(curr_id):
 		_automatic_bgmusic_pick_and_play()
