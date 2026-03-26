c5015f7334bd7a4756b0802f45e747cd90f8eb85
thatdev <anthony@thatdev.org>
2026-03-26 19:16:57 +1100

    a

--- scripts/MusicBank.gd
+++ scripts/MusicBank.gd
@@ -12,12 +12,19 @@ enum Automatic { DISABLED, BGMUSIC }
 @export var repeatMode: Mode = Mode.THIRD
 
 
-const INVALID_INDEX := -40001
+const INVALID_INDEX := MusicManager.INVALID_INDEX
 
 var max_wait: int = 0
 var last_played: Array[String] = [] # store track names
 var curr_id: int = INVALID_INDEX
 
+var auto_playing: bool = true
+
+enum ListenerType { PLAY_TRACK }
+
+var listeners: Dictionary[ListenerType, Array] = { # nested collection doesn't work in gdscript
+	ListenerType.PLAY_TRACK: [],
+}
 
 func _get_track_by_name(name: String) -> MusicTrack:
 	for t in tracks:
@@ -61,6 +68,8 @@ func play_track(name: String) -> bool:
 		MusicManager.remove(curr_id)
 
 	curr_id = MusicManager.play(track.stream)
+	for v in listeners[ListenerType.PLAY_TRACK]:
+		v.call()
 	return true
 
 func fetch_id() -> int:
@@ -72,6 +81,15 @@ func fetch_player() -> AudioStreamPlayer:
 	if curr_id == INVALID_INDEX:
 		return null
 	return MusicManager.get_player(curr_id)
+
+func stop_auto(stop_curr_music: bool = true, del_curr_music: bool = true) -> void:
+	auto_playing = false
+	if curr_id == INVALID_INDEX:
+		return
+	if stop_curr_music:
+		MusicManager.stop(curr_id)
+	if del_curr_music:
+		MusicManager.remove(curr_id)
 
 func _ready() -> void:
 	if automatic == Automatic.BGMUSIC:
@@ -86,6 +104,8 @@ func _ready() -> void:
 
 	_automatic_bgmusic_pick_and_play()
 
+func add_listener(listener_type: ListenerType, listener: Callable):
+	listeners[listener_type].append(listener)
 
 func _process(delta: float) -> void:
 	if curr_id == INVALID_INDEX or not MusicManager.get_playing(curr_id):
